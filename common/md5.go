9e310c4e4eb2580718f1aed35b89ddab314b318c
xunull <todogo@vip.163.com>
2021-10-23 18:15:47 +0800

    update

--- common/md5.go
+++ common/md5.go
@@ -11,6 +11,12 @@ func MD5(str []byte) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+func StrMd5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
 func MD5Salt(str, salt string) string {
 	return MD5([]byte(str + salt))
 }
