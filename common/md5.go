60912db95a88f54629e07bb8d7c31b88fb8c837d
xunull <todogo@vip.163.com>
2021-11-12 15:15:52 +0800

    update

--- common/md5.go
+++ common/md5.go
@@ -3,6 +3,10 @@ package common
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
 )
 
 func MD5(str []byte) string {
@@ -19,4 +23,20 @@ func StrMd5(str string) string {
 
 func MD5Salt(str, salt string) string {
 	return MD5([]byte(str + salt))
+}
+
+func MD5File(filepath string) (string, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	body, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	md5 := fmt.Sprintf("%x", md5.Sum(body))
+	runtime.GC()
+	return md5, nil
 }
