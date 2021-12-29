f7cf15a65fc9d980320440b6b9080f1c3e9fc406
xunull <todogo@vip.163.com>
2021-12-29 10:04:54 +0800

    update

--- git_cmd/status.go
+++ git_cmd/status.go
@@ -1,7 +1,9 @@
 package git_cmd
 
 import (
+	"bufio"
 	"github.com/xunull/goc/commandx"
+	"io"
 	"strings"
 )
 
@@ -40,6 +42,29 @@ func (g *GitApi) CheckUpToDate(all bool) (bool, *commandx.CommandResult) {
 		}
 	} else {
 		return false, res
+	}
+}
+
+func (g *GitApi) StatusPorcelain() ([]string, error) {
+	cmd := []string{"git", "status", "--porcelain=1"}
+
+	cmdRes := commandx.RunCommand(cmd, commandx.WithDir(g.Dir))
+	if cmdRes.Success {
+		lines := make([]string, 0)
+		buff := bufio.NewReader(&cmdRes.Stdout)
+		for {
+			line, err := buff.ReadString('\n')
+			if io.EOF == err {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+			lines = append(lines, line)
+		}
+		return lines, nil
+	} else {
+		return nil, cmdRes.Err
 	}
 }
 
